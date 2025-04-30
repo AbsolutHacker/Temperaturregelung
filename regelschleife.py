d205ff12896daa3a6ab4c4f7f6494ab139eaa2d9
Nick Seufert <nicolas.seufert@gmail.com>
2025-04-30 10:44:19 +0200

    formatting and logging (first prototype)

--- regelschleife.py
+++ regelschleife.py
@@ -1,15 +1,23 @@
 import time
+import sys
 import sensor as sensor_module
 from actuators import RelayActuator
 import rules
+import logging as log
+import formatters
 
 
 def main():
-    print('Initialisiere Sensor...')
+    log.debug('Initialisiere Sensor...')
     sensor = sensor_module.BME280Sensor()
 
-    print('Initialisiere Aktoren...')
+    log.debug('Initialisiere Aktoren...')
     actuators = [ RelayActuator() ] #, ZigbeeActuator() ]
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--csv':
+        formatter = formatters.CSVFormatter()
+    else:
+        formatter = formatters.ConsoleFormatter()
 
     # actuators are on per default
     last_actuator_on_state = True
@@ -20,17 +28,28 @@ def main():
     while True:
         try:
             temperature = sensor.read_temperature()
-            print(f'Temperatur: {temperature:.2f} °C')
+            humidity = sensor.read_humidity()
+            pressure = sensor.read_pressure()
+            datetime = time.localtime()
+            log.info(formatter.format(datetime, temperature, humidity, pressure))
             switch_off = rules.should_switch_off_for_temp(temperature)
         except Exception as exception:
-            print(f'[WARN] Fehler beim Auslesen des Sensors: {exception}')
-            print(f'[WARN] -> Kühlung an')
+            log.warn(f'Fehler beim Auslesen des Sensors: {exception}')
+            log.warn(f'-> Kühlung an')
             switch_off = False
+
         if switch_off == last_actuator_on_state:
             for actuator in actuators:
                 actuator.set_state(switch_off)
             last_actuator_on_state = not switch_off
-        time.sleep(rules.interval_in_s)
+
+        try:
+            time.sleep(rules.interval_in_s)
+        except KeyboardInterrupt:
+            log.debug('Unterbrechung, schalte ab')
+            for actuator in actuators:
+                actuator.destroy()
+            break
 
 
 if __name__ == '__main__':
