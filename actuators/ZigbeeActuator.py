96c0e9f1f4ff0800f0aecd8ae10cfac26903e8fb
Nick Seufert <nicolas.seufert@gmail.com>
2025-05-03 19:01:58 +0200

    flesh out Zigbee actuator, z2m install script/unit file

--- actuators/ZigbeeActuator.py
+++ actuators/ZigbeeActuator.py
@@ -1,13 +1,20 @@
 import logging as log
-import zigpy
 
 
 class ZigbeeActuator:
-    def __init__(self):
+    def __init__(self, device_name: str = 'steckdose'):
+        import paho.mqtt.client as mqtt
         log.debug(f'Initialisiere Zigbee-Aktor')
+        self.topic = f"zigbee2mqtt/{device_name}/set"
+        self.client = mqtt.Client()
+        self.client.connect("localhost", 1883, 60)
 
-    def set_state(self, off):
+    def set_state(self, off: bool):
+        import json
         log.debug(f"ZigbeeAktor.schalte({'aus' if off else 'an'})")
+        payload = json.dumps({'state': 'OFF' if off else 'ON'})
+        self.client.publish(self.topic, payload)
 
     def destroy(self):
-        pass
+        log.debug("Vernichte Zigbee-Aktor")
+        self.client.disconnect()
